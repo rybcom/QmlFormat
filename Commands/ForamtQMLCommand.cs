a088c69524a4f30b7db8c2cb37a06e9a13ded252
michal rybarik <michal.rybarik@gmail.com>
2023-03-04 22:44:10 +0100

    initial commit

---
+++ Commands/ForamtQMLCommand.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+
+namespace QmlFormat
+{
+    internal static class OptionsHelper
+    {
+        public async static Task<string> GetArgsAsync(string filePath)
+        {
+            var options = await General.GetLiveInstanceAsync();
+
+            var verbose = options.Verbose ? "-V" : "";
+            var force = options.Force ? "-f" : "";
+            var tabs = options.Tabs ? "-t" : "";
+            var indentWidth = $"-w {options.IndentWidth}";
+            var normalize = options.Normalize ? "-n" : "";
+
+            string args;
+            if (options.QtVersion == QtVersion.Qt5)
+                args = $"{verbose} {normalize} {force} -i \"{filePath}\"";
+            else if (options.QtVersion == QtVersion.Qt6)
+                args = $"{verbose} {normalize} {force} {tabs} {indentWidth} -i \"{filePath}\"";
+            else
+                args = $"{verbose} {normalize} {force} {tabs} {indentWidth} -i \"{filePath}\"";
+
+            return args;
+        }
+
+        public async static Task<string> GetFormatPathAsync()
+        {
+            var options = await General.GetLiveInstanceAsync();
+            return Environment.ExpandEnvironmentVariables(options.QmlFormatPath);
+        }
+    }
+
+    [Command(PackageIds.FormatQMLCommand)]
+    internal sealed class FormatQMLCommand : BaseCommand<FormatQMLCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            if (docView != null)
+            {
+                var filePath = docView.FilePath;
+                if (filePath != null)
+                {
+                    try
+                    {
+                        var process = new System.Diagnostics.Process();
+                        process.StartInfo.FileName = await OptionsHelper.GetFormatPathAsync();
+                        process.StartInfo.Arguments = await OptionsHelper.GetArgsAsync(filePath);
+                        process.Start();
+                    }
+                    catch (Exception ex)
+                    {
+                        await VS.MessageBox.ShowErrorAsync("qmlformat error", ex.Message);
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Options/General.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace QmlFormat
+{
+    internal partial class OptionsProvider
+    {
+        // Register the options with this attribute on your package class:
+        // [ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), "QmlFormat", "General", 0, 0, true, SupportsProfiles = true)]
+        [ComVisible(true)]
+        public class GeneralOptions : BaseOptionPage<General> { }
+    }
+
+    public enum QtVersion
+    {
+        Qt5,
+        Qt6
+    }
+
+    public class General : BaseOptionModel<General>
+    {
+        [Category("qmlformat")]
+        [DisplayName("Qt version")]
+        [Description("version of installed qt libraries")]
+        [DefaultValue(QtVersion.Qt6)]
+        public QtVersion QtVersion { get; set; }
+
+        [Category("qmlformat")]
+        [DisplayName("qmlformat.exe path")]
+        [Description("Full path to qmlformat.exe")]
+        [DefaultValue("")]
+        public string QmlFormatPath { get; set; }
+
+        [Category("option")]
+        [DisplayName("verbose")]
+        [Description("--verbose option")]
+        [DefaultValue(true)]
+        public bool Verbose { get; set; }
+
+        [Category("option")]
+        [DisplayName("force")]
+        [Description("--force option")]
+        [DefaultValue(true)]
+        public bool Force { get; set; }
+
+        [Category("option")]
+        [DisplayName("tabs")]
+        [Description("--tabs option")]
+        [DefaultValue(false)]
+        public bool Tabs { get; set; }
+
+        [Category("option")]
+        [DisplayName("indent width")]
+        [Description("--indent-width option")]
+        [DefaultValue(4)]
+        public uint IndentWidth { get; set; }
+
+        [Category("option")]
+        [DisplayName("normalize/no sort")]
+        [Description("--normalize option in Qt6   --no-sort imports in Qt5")]
+        [DefaultValue(false)]
+        public bool Normalize { get; set; }
+    }
+}
